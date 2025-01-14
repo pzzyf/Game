daf68fa517f7b931d0bb63d90ee51520e3141023
pzzyf <2279948211@qq.com>
2025-01-14 22:32:46 +0800

    ✅  交替落子  是否下过  判断胜利  胜利状态

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,15 +6,29 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
-
+  const [xIsNext, setXIsNext] = useState(true)
+  const winner = calculateWinner(squares)
   function handleClk(i: number) {
+    if (squares[i] || calculateWinner(squares)) {
+      return
+    }
+
     const temp = squares.slice()
-    temp[i] = 'X'
+    temp[i] = xIsNext ? 'X' : 'O'
     setSquares(temp)
+    setXIsNext(!xIsNext)
   }
 
   return (
     <>
+      {winner
+        ? (
+            <div>
+              Winner:
+              {winner}
+            </div>
+          )
+        : null}
       <div>
         <Square value={squares[0]} onSquareClick={() => handleClk(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClk(1)} />
@@ -32,4 +46,25 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
